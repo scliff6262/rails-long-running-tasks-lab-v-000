5c8cf502f832a7251351559798c4f15c097d430a
Sean Clifford <sclifford6262@gmail.com>
2018-06-07 18:11:38 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -46,8 +46,8 @@ class SongsController < ApplicationController
   end
 
   def upload
-    CSV.foreach(params[:songs].path, headers: true) do |lead|
-      Song.create()
+    CSV.foreach(params[:songs].path, headers: true) do |song|
+      Song.create(title: song[0], artist_name: song[1])
     end
   end 
 
